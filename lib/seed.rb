94168eb91f6a32373a759650b44dbc4b407e64eb
Eric Haughee <ehaughee@gmail.com>
2013-05-10 18:09:14 -0700

    Added back link and additional seed data

--- lib/seed.rb
+++ lib/seed.rb
@@ -1,6 +1,12 @@
 timer = Timer.new(
   name:   "Test",
-  end:    Date.new(2013,12,31)
+  end:    Date.new(2013, 12, 31)
+)
+
+timer2 = Timer.new(
+  name:  "Test2", 
+  end:   Date.new(2013, 6, 30)
 )
 
 timer.save!
+timer2.save!
